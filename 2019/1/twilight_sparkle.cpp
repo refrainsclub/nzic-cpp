d3cc8d9d9323159fc8309c615e8d94ce72ae2b4e
James Blair <james@blair.nz>
2024-03-18 16:38:34 +1300

    twilight sparkle p2

---
+++ 2019/1/twilight_sparkle.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+typedef unsigned long long int ulli;
+
+int main() {
+  ulli a, b, c, d, n, u, v;
+  std::cin >> a >> b >> c >> d >> n;
+
+  u = v = 1;
+
+  for (ulli i = 0; i < n; i++) {
+    ulli tmp = u;
+    u = (a * u + b * v) % 10'000;
+    v = (c * tmp + d * v) % 10'000;
+  }
+
+  std::cout << u % 10'000 << ' ' << v % 10'000 << '\n';
+  return 0;
+}
